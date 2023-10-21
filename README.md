ef0c6a4db41205a71ea1d529168683bc40424f24
SOutaHI <hirose.sgia@gmail.com>
2023-10-22 08:42:25 +0900

    🚧  debugging on download process

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ npm start
 - [Can't export fromIni from @aws-sdk/credential-providers](https://stackoverflow.com/questions/72088326/cant-export-fromini-from-aws-sdk-credential-providers)
 - [type checking error with fromEnv in @aws-sdk/credential-providers](https://github.com/aws/aws-sdk-js-v3/issues/3392)
 
-### arrow CORS
+### allow CORS
 - [S3とCloudfrontを使った際にCORSエラーを解消する方法](https://zenn.dev/okita_kamegoro/scraps/17cd1e1653cb57)
 - [Amazon S3 で CORS を設定し、cURL を使用して CORS ルールを確認する方法](https://repost.aws/ja/knowledge-center/s3-configure-cors)
 
@@ -33,3 +33,11 @@ npm start
 
 - [AWS SDK を使用して Amazon S3 オブジェクトを一覧表示するウェブページ](https://docs.aws.amazon.com/ja_jp/AmazonS3/latest/userguide/example_s3_Scenario_ListObjectsWeb_section.html)
 - [AWS SDK for JavaScript（Version 3） と TypeScript を用いて S3 バケットの一覧を取得するサンプルコードを試してみた](https://dev.classmethod.jp/articles/aws-sdk-for-javascript-v3-typescript/)
+
+### download s3 object
+- [how to download file in react js](https://stackoverflow.com/questions/50694881/how-to-download-file-in-react-js)
+- [How to download the file from server location in react Js](https://stackoverflow.com/questions/58830976/how-to-download-the-file-from-server-location-in-react-js)
+- [Download file in React JS | Download a File or Image using React](https://www.youtube.com/watch?v=zslesB7EWs0)
+- [How to download a File in React.js (local or from URL)](https://bobbyhadz.com/blog/react-download-file)
+- [AWS-SDK v3 - Download file with Typescript](https://stackoverflow.com/questions/68373349/aws-sdk-v3-download-file-with-typescript)
+- [S3.GetObject no longer returns the result as a string](https://github.com/aws/aws-sdk-js-v3/issues/1877)
--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,12 @@ import {
   ListObjectsCommand,
   ListObjectsCommandOutput,
   S3Client,
+  GetObjectCommand,
 } from "@aws-sdk/client-s3";
+
+import { createWriteStream } from "fs";
+import { Readable } from "stream";
+
 import "./App.css";
 
 function App() {
@@ -31,24 +36,43 @@ function App() {
     });
   }, []);
 
+  const downloadFileOnFileSystem = async () => {
+    const client = new S3Client({
+      region: process.env.REACT_APP_AWS_DEFAULT_REGION as string,
+      credentials: {
+        accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY as string,
+      },
+    });
+
+    const s3Object = await client.send(
+      new GetObjectCommand({
+        Bucket: process.env.REACT_APP_BUCKET_NAME,
+        Key: objects[0].Key as string,
+      })
+    );
+    console.log(s3Object);
+
+    let file = require("fs").createWriteStream("~/Download/" + objects[0].Key as string);
+    // s3.getObject(params).createReadStream().pipe(file);
+
+    // await new Promise((resolve, reject) => {
+    //   if (s3Object.Body instanceof Readable) {
+    //     s3Object.Body.pipe(createWriteStream(objects[0].Key as string))
+    //       .on("error", (err) => reject(err))
+    //       .on("close", () => resolve(0));
+    //   }
+    // });
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         {objects.map((o) => (
           <div key={o.ETag}>{o.Key}</div>
         ))}
+        <button onClick={() => downloadFileOnFileSystem()}>Download</button>
       </header>
     </div>
   );

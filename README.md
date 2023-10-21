bf8b7ff56e1ab7e4d4c08d4d7abaee00bd153695
SOutaHI <hirose.sgia@gmail.com>
2023-10-21 20:45:05 +0900

    ✨ confirmed access s3 from typescript

--- README.md
+++ README.md
@@ -1,5 +1,35 @@
 # get-ObjectList-from-s3
 
-# ref
+## setup
+- create .env file, and write access key for s3.
+```sh
+REACT_APP_AWS_ACCESS_KEY_ID='*************'
+REACT_APP_AWS_SECRET_ACCESS_KEY='**********'
+REACT_APP_AWS_DEFAULT_REGION='ap-northeast-1'
+REACT_APP_BUCKET_NAME='***************'
+```
+
+## execute
+
+```sh
+npm start
+```
+
+## ref
+### uniform info
+- [Amazon S3 examples using SDK for JavaScript (v3)](https://docs.aws.amazon.com/sdk-for-javascript/v3/developer-guide/javascript_s3_code_examples.html)
+- [@aws-sdk/credential-providers](https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/Package/-aws-sdk-credential-providers/#fromenv)
+- [aws-sdk/client-s3 ListBucketsCommand](https://docs.aws.amazon.com/AWSJavaScriptSDK/v3/latest/client/s3/command/ListBucketsCommand/)
+
+### authorize
+- [Can't export fromIni from @aws-sdk/credential-providers](https://stackoverflow.com/questions/72088326/cant-export-fromini-from-aws-sdk-credential-providers)
+- [type checking error with fromEnv in @aws-sdk/credential-providers](https://github.com/aws/aws-sdk-js-v3/issues/3392)
+
+### arrow CORS
+- [S3とCloudfrontを使った際にCORSエラーを解消する方法](https://zenn.dev/okita_kamegoro/scraps/17cd1e1653cb57)
+- [Amazon S3 で CORS を設定し、cURL を使用して CORS ルールを確認する方法](https://repost.aws/ja/knowledge-center/s3-configure-cors)
+
+### example
+
 - [AWS SDK を使用して Amazon S3 オブジェクトを一覧表示するウェブページ](https://docs.aws.amazon.com/ja_jp/AmazonS3/latest/userguide/example_s3_Scenario_ListObjectsWeb_section.html)
 - [AWS SDK for JavaScript（Version 3） と TypeScript を用いて S3 バケットの一覧を取得するサンプルコードを試してみた](https://dev.classmethod.jp/articles/aws-sdk-for-javascript-v3-typescript/)
--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,6 @@ import {
   ListObjectsCommandOutput,
   S3Client,
 } from "@aws-sdk/client-s3";
-import { fromCognitoIdentityPool } from "@aws-sdk/credential-providers";
-
 import "./App.css";
 
 function App() {
@@ -18,30 +16,19 @@ function App() {
 
   useEffect(() => {
     const client = new S3Client({
-      region: "us-east-1",
-      // Unless you have a public bucket, you'll need access to a private bucket.
-      // One way to do this is to create an Amazon Cognito identity pool, attach a role to the pool,
-      // and grant the role access to the 's3:GetObject' action.
-      //
-      // You'll also need to configure the CORS settings on the bucket to allow traffic from
-      // this example site. Here's an example configuration that allows all origins. Don't
-      // do this in production.
-      //[
-      //  {
-      //    "AllowedHeaders": ["*"],
-      //    "AllowedMethods": ["GET"],
-      //    "AllowedOrigins": ["*"],
-      //    "ExposeHeaders": [],
-      //  },
-      //]
-      //
-      credentials: fromCognitoIdentityPool({
-        clientConfig: { region: "us-east-1" },
-        identityPoolId: "<YOUR_IDENTITY_POOL_ID>",
-      }),
+      region: process.env.REACT_APP_AWS_DEFAULT_REGION as string,
+      credentials: {
+        accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID as string,
+        secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY as string,
+      },
     });
-    const command = new ListObjectsCommand({ Bucket: "bucket-name" });
-    client.send(command).then(({ Contents }) => setObjects(Contents || []));
+    const command = new ListObjectsCommand({
+      Bucket: process.env.REACT_APP_BUCKET_NAME as string,
+    });
+    client.send(command).then(({ Contents }) => {
+      console.log(Contents);
+      setObjects(Contents || []);
+    });
   }, []);
 
   return (
@@ -59,10 +46,10 @@ function App() {
         >
           Learn React
         </a>
+        {objects.map((o) => (
+          <div key={o.ETag}>{o.Key}</div>
+        ))}
       </header>
-      {objects.map((o) => (
-        <div key={o.ETag}>{o.Key}</div>
-      ))}
     </div>
   );
 }
